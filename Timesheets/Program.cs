fc329cc915a334825ab26718eb8f8b2495c2b9d9
George Sovatzis <gsovatzis@gmail.com>
2019-11-24 16:09:45 +0200

    Update Program.cs

--- Timesheets/Program.cs
+++ Timesheets/Program.cs
@@ -13,7 +13,7 @@ namespace Timesheets
     {
         public static void Main(string[] args)
         {
-            // This is a change - gsova!
+            // This is a change - gsova! - this is a feature branch change
             CreateHostBuilder(args).Build().Run();
         }
 
