2d497b5f39c5d00f615f0932ffe970faf020b805
Aris <ariskallergis1@gmail.com>
2019-12-13 03:04:58 +0200

    Made user management index page

---
+++ Timesheets/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Timesheets.Data;
+
+namespace Timesheets.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Users;
+            return View(await applicationDbContext.ToListAsync());
+        }
+    }
+}
