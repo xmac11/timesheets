0de9b0dd283908cfe4520e16074bed1a7a3581eb
Iosif <12469gr@saeinstitute.edu>
2019-12-14 16:54:27 +0200

    Authorization in TimeSheetEntriesController

--- Timesheets/Controllers/TimesheetEntriesController.cs
+++ Timesheets/Controllers/TimesheetEntriesController.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using Timesheets.Data;
 using Timesheets.Mappers;
 using Timesheets.Models;
 
 namespace Timesheets.Controllers
 {
+    [Authorize(Roles = "Admin,Manager,Employee")]
     public class TimesheetEntriesController : Controller
     {
         private readonly ApplicationDbContext _context;
